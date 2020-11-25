24cf57bceca87b278d68f6d4a5b5de27e299ea83
230443 Daniel Baraniak <230443@edu.p.lodz.pl>
2020-11-25 22:50:22 +0100

    add release target, add test for time

--- bmpedit/bmpedit/Test/BitmapTest.cpp
+++ bmpedit/bmpedit/Test/BitmapTest.cpp
@@ -4,30 +4,18 @@
 
 #include <boost/test/unit_test.hpp>
 #include <cmath>
+#include <string>
 #include "Bitmap.h"
+#include "Task2.cpp"
 #include <boost/date_time/posix_time/posix_time.hpp>
-#define IMAGE "lena_impulse2.bmp"
+#define IMAGE "/run/media/daniel/Shared/Images/lena/lena.bmp"
 inline boost::posix_time::ptime now()
 {
     return boost::posix_time::microsec_clock::local_time();
 }
-
+//std::string name("lena.bmp");
 
 BOOST_AUTO_TEST_SUITE(BitmapSuite)
-
-    BOOST_AUTO_TEST_CASE(cmeanTime)
-    {
-        Bitmap img(IMAGE);
-
-        boost::posix_time::ptime startTime = now();
-
-        img.cmean(5,2);
-
-        boost::posix_time::time_duration elapsed = now() - startTime;
-
-        std::cout << "cmean " << elapsed << std::endl;
-
-    }
 
     BOOST_AUTO_TEST_CASE(FilterCmean_Time)
     {
@@ -40,19 +28,6 @@ BOOST_AUTO_TEST_SUITE(BitmapSuite)
         boost::posix_time::time_duration elapsed = now() - startTime;
 
         std::cout << "filter_contra " << elapsed << std::endl;
-
-    }
-    BOOST_AUTO_TEST_CASE(Alpha_Time)
-    {
-        Bitmap img(IMAGE);
-
-        boost::posix_time::ptime startTime = now();
-
-        img.alpha(16,2);
-
-        boost::posix_time::time_duration elapsed = now() - startTime;
-
-        std::cout << "alpha " << elapsed << std::endl;
 
     }
     BOOST_AUTO_TEST_CASE(FilterAlpha_Time)
@@ -71,7 +46,7 @@ BOOST_AUTO_TEST_SUITE(BitmapSuite)
 
     BOOST_AUTO_TEST_CASE(exponential)
     {
-        Bitmap img("lena.bmp");
+        Bitmap img(IMAGE);
         img.contrast(0.5);
         img.save("contrast.bmp");
         img.image.display("contrast",false,0,true);
@@ -83,17 +58,77 @@ BOOST_AUTO_TEST_SUITE(BitmapSuite)
 
     }
 
-    */
     BOOST_AUTO_TEST_CASE(histogram)
     {
-        Bitmap img("lena.bmp");
+        Bitmap img(IMAGE);
         img.histogram();
     }
     BOOST_AUTO_TEST_CASE(mean)
     {
-        Bitmap img("lena.bmp");
+        Bitmap img(IMAGE);
         std::cout<<img.mean()<<std::endl;
         std::cout<<img.casyco();
     }
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(laplaceSuite)
+
+	int n = 1000;
+	BOOST_AUTO_TEST_CASE(slaplace0)
+	{
+		boost::posix_time::time_duration sum;
+		for (int i = 0; i < n; ++i)
+		{
+			Bitmap img(IMAGE);
+			boost::posix_time::ptime startTime = now();
+
+			img.slaplace(nullptr);
+
+			boost::posix_time::time_duration elapsed = now() - startTime;
+			sum += elapsed;
+		}
+
+		std::cout << "slaplace0 " << sum/n << std::endl;
+
+	}
+	BOOST_AUTO_TEST_CASE(slaplace1)
+	{
+		boost::posix_time::time_duration sum;
+		for (int i = 0; i < n; ++i)
+		{
+			Bitmap img(IMAGE);
+			boost::posix_time::ptime startTime = now();
+
+			int mask[9] = { -1, -1, -1,
+					-1, +8, -1,
+					-1, -1, -1 };
+			img.slaplace(mask);
+
+			boost::posix_time::time_duration elapsed = now() - startTime;
+			sum += elapsed;
+		}
+
+		std::cout << "slaplace1 " << sum/n << std::endl;
+
+	}
+	BOOST_AUTO_TEST_CASE(slaplace2)
+	{
+		boost::posix_time::time_duration sum;
+		for (int i = 0; i < n; ++i)
+		{
+			Bitmap img(IMAGE);
+			boost::posix_time::ptime startTime = now();
+
+			int mask[9] = { +1, -2, +1,
+					-2, +4, -2,
+					+1, -2, +1 };
+			img.slaplace(mask);
+
+			boost::posix_time::time_duration elapsed = now() - startTime;
+			sum += elapsed;
+		}
+		std::cout << "slaplace2 " << sum/n << std::endl;
+	}
 
 BOOST_AUTO_TEST_SUITE_END()
